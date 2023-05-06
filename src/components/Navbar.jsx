6877f8f8152bd00bd89dcadd8edadeaca68f298a
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-05-05 23:47:40 -0500

    Starting Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import { styles } from '../styles';
-import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-
-  const [active, setActive] = useState('');
+  const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
 
   console.log(toggle);
@@ -16,32 +15,31 @@ const Navbar = () => {
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
-
+      <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
         {/* Link de la imagen y texto para que diriga a oagina principal */}
         {/* ----------------------------------------------- */}
         <Link
           to="/"
-          className='flex items-center gap-2'
+          className="flex items-center gap-2"
           onClick={() => {
             setActive("");
-            window.scrollTo(0,0); //va a hacer que se escrolee al top de la pantalla
+            window.scrollTo(0, 0); //va a hacer que se escrolee al top de la pantalla
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Nemecio <span className='sm:block hidden'>| Portafolio</span></p>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Nemecio &nbsp; <span className="sm:block hidden">| Portafolio</span>
+          </p>
         </Link>
 
         {/* botones de navegacion en la parte derecha del navbar */}
         {/* ----------------------------------------------- */}
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
-          { navLinks.map((link) => (
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
             <li
               key={link.id}
               className={`${
-                active === link.title
-                  ? "text-white"
-                  : "text-secondary"
+                active === link.title ? "text-white" : "text-secondary"
               } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(link.title)}
             >
@@ -53,36 +51,39 @@ const Navbar = () => {
         {/* Opcion donde se comprimen los botones de navegacion en small deivices */}
         {/* ----------------------------------------------- */}
 
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
             src={toggle ? close : menu}
-            alt='menu'
-            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
-          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-          
-          <ul className='list-none flex justify-end items-start flex-col gap-4'>
-            { navLinks.map((link) => (
-              <li
-                key={link.id}
-                className={`${
-                  active === link.title
-                    ? "text-white"
-                    : "text-secondary"
-                } hover:text-white text-[18px] font-medium cursor-pointer`}
-                onClick={() => setActive(link.title)}
-              >
-                <a href={`#${link.id}`}>{link.title}</a>
-              </li>
-            ))}
-          </ul>
-
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!toggle);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
