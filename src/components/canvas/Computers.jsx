33800b74332c20720ace502ffdb5bceaac5e07b3
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-05-09 21:41:59 -0500

    Canvas Loader

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -15,16 +15,25 @@ const Computers = () => {
         groundColor="black" 
       />
       <pointLight intensity={1}/>
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
         scale={0.75}
         position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
-}
+};
 
-const ComputerCanvas = () => {
+const ComputersCanvas = () => {
   return (
     <Canvas
       frameloop='demand'
@@ -45,4 +54,4 @@ const ComputerCanvas = () => {
   )
 }
 
-export default Computers;
+export default ComputersCanvas;
