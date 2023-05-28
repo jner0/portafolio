0dda41065662c8cafaae8637c1d020d1434e46d5
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-05-28 18:12:58 -0500

    Starting work section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Tilt from "react-parallax-tilt";
+import { motion } from 'framer-motion';
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Mis proyectos</p>
+        <h2 className={styles.sectionHeadText}>Proyectos..</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Escribir, que a continuacion se mostraran los proyectos que demuestran mis habilidades o conocimientos. blablabal cada proyecto esta con su descripcion blablabal y con sus links a repositorio .
+          Esto demuestra mis blbalbalba con diferentes tecnologias y blablabal
+        </motion.p>
+      </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "")
