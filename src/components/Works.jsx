7e94d0a303a256806a5bf005868f62827b0c8e8b
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-05-28 20:26:53 -0500

    Work section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,6 +7,57 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ProjectCard = ({ index, name, description, tags, image, source_code_link}) => {
+  return(
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary p-5 rounded-2x1 sm:w-[360px] w-full"
+        style={{ borderRadius: "20px" }}
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover"
+            style={{ borderRadius: "20px" }}
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open
+              (source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img 
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  )
+}
+
 const Works = () => {
   return (
     <>
@@ -23,6 +74,17 @@ const Works = () => {
           Escribir, que a continuacion se mostraran los proyectos que demuestran mis habilidades o conocimientos. blablabal cada proyecto esta con su descripcion blablabal y con sus links a repositorio .
           Esto demuestra mis blbalbalba con diferentes tecnologias y blablabal
         </motion.p>
+      </div>
+      
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard 
+            key={`project-${index}`}
+            index={index}
+            {...project}
+          />
+        ))}
+
       </div>
     </>
   )
