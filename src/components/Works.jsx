ebfc2df4ef5fce08a2f275a55f6f635250769426
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-05-29 21:52:21 -0500

    FeedBacks and Contacts section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -16,15 +16,15 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link})
           scale: 1,
           speed: 450
         }}
-        className="bg-tertiary p-5 rounded-2x1 sm:w-[360px] w-full"
-        style={{ borderRadius: "20px" }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        
       >
-        <div className="relative w-full h-[230px]">
+        <div className="relative w-full h-[230px] ">
           <img
             src={image}
             alt={name}
-            className="w-full h-full object-cover"
-            style={{ borderRadius: "20px" }}
+            className="w-full h-full object-cover rounded-2xl"
+            
           />
 
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
